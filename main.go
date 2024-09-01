afeb71d65cba19a4faaf12abd35fba4fe7ec58ad
Ryan <ryanisaacg@users.noreply.github.com>
2024-09-01 17:55:16 -0400

    Basic multi-file selector / uploader

--- main.go
+++ main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello, world.")
+	// Upload media
+	http.HandleFunc("/media", func(w http.ResponseWriter, r *http.Request) {
+		r.ParseMultipartForm(1024 * 1024 * 16)
+		multipartFormData := r.MultipartForm
+		fmt.Println(multipartFormData.File)
+		files := 0
+		for _, v := range multipartFormData.File["music"] {
+			fmt.Println(v.Filename, ":", v.Size)
+			uploadedFile, _ := v.Open()
+			_, err := io.ReadAll(uploadedFile)
+			uploadedFile.Close()
+			if err != nil {
+				fmt.Println("err", err)
+			} else {
+				files += 1
+			}
+		}
+		fmt.Println(files)
+		http.Redirect(w, r, fmt.Sprintf("?files=%d", files), http.StatusSeeOther)
+		w.Write([]byte("Redirecting - if you aren't redirected, <a href=\"/\"> click here </a>"))
+	})
+
+	// Serve website
+	fs := http.FileServer(http.Dir("static/"))
+	http.Handle("/", fs)
+
+	fmt.Println("listening on 8085")
+	http.ListenAndServe(":8085", nil)
 }
